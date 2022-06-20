0f1de066e50df566f795fc3287507b9336b71abd
Rodger Oliver <timeline.mb@gmail.com>
2022-06-20 10:03:25 -0300

    Add default user icon if profile picture is not given on navigation.blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,8 +23,15 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div class="w-10 h-10 rounded-full overflow-hidden shadow-lg scale-110 flex items-center mr-3">
-                                <img class="w-full h-full object-cover" src="{{ asset('uploads/profile_pictures/' . Auth::user()->profile_picture) }}" />
+                            <div class="w-8 h-8 rounded-full overflow-hidden shadow-lg scale-110 flex items-center mr-3">
+                                @if(Auth::user()->profile_picture)
+                                    <img class="w-full h-full object-cover" src="{{ asset('uploads/profile_pictures/' . Auth::user()->profile_picture) }}" />
+                                @else
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle w-full h-full" viewBox="0 0 16 16">
+                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                                    </svg>
+                                @endif
                             </div>
                             <div>{{ Auth::user()->name }}</div>
 
