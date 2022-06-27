a6fdc31f2dac909a1c46ecfa74cfc27a65a532c1
Rodger Oliver <timeline.mb@gmail.com>
2022-06-27 15:22:07 -0300

    Populate user log fields with the Model when creating and updating a task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Task extends Model
 {
@@ -12,4 +13,18 @@ class Task extends Model
     protected $fillable = [
         'name'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        if(Auth::check()) {
+            self::creating(function($model) {
+                $model->created_by = Auth::id();
+                $model->updated_by = Auth::id();
+            });
+            self::updating(function($model) {
+                $model->updated_by = Auth::id();
+            });
+        }
+    }
 }
