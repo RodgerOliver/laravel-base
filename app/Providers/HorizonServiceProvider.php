e33cc7b6129971f8f9f0010d31792b73d29bb7a7
Rodger Oliver <timeline.mb@gmail.com>
2022-06-28 12:58:48 -0300

    Add documentation about user access on "gate" method from HorizonServiceProvider

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,6 +28,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      * Register the Horizon gate.
      *
      * This gate determines who can access Horizon in non-local environments.
+     * If none users are provided bellow, only user with id 1 can access.
      *
      * @return void
      */
