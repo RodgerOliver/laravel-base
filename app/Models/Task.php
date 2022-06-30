088a7387ac20bf976a27f1c2214ebf6d6a29163b
Rodger Oliver <timeline.mb@gmail.com>
2022-06-30 11:34:15 -0300

    Add FilberByUser trait to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\FilterByUser;
 use App\Traits\LogUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    use HasFactory, LogUser;
+    use HasFactory, LogUser, FilterByUser;
 
     protected $fillable = [
         'name'
