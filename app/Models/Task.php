1893a062a3f3eb4495c0df9ff59ff1e4fef04a3c
Rodger Oliver <timeline.mb@gmail.com>
2022-07-06 12:52:34 -0300

    Enable Task model to be searchable

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,10 +7,11 @@ use App\Traits\FilterByUser;
 use App\Traits\LogUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Task extends Model
 {
-    use HasFactory, LogUser, FilterByUser;
+    use HasFactory, LogUser, FilterByUser, Searchable;
 
     protected $fillable = [
         'name'
